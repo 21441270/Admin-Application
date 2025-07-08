950452cf422c965d2e68c01392f2581e06f2e14a
Alisha Mahmood <21441270@stu.mmu.ac.uk>
2025-07-08 12:05:08 +0100

    Adding Code Back In DB File

--- backend/db.php
+++ backend/db.php
@@ -11,7 +11,7 @@
  // Check connection
  if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
- } /* else {
-    echo "Connected successfully";
- } */
-?>
+ } else {
+      echo "Connected successfully";
+ }
+ ?>
